54c0aac58b1688f96d309707063eac9889b53516
BasCart <b.cartagena.paredes@gmail.com>
2025-03-22 15:15:30 +0000

    add todolist

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,55 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
-const Home = () => {
+export const Home = () => {
+	let [item, setItem] = useState("")
+	let [listTarea, setListTarea] = useState([])
+	const[hover, setHover]= useState(false);
+	const estilo = { backgroundColor: hover ? "lightgray" : "rgb(248, 249, 250)" }
+	const[counter, setCounter] = useState(0)
+	const add=()=> {setCounter ((prevCounter) => prevCounter + 1)}
+	const sub=()=> {setCounter ((prevCounter) => prevCounter - 1)}
+	
+	const handleChange = (event) => {
+		setItem(event.target.value)
+	}
+	const deleteItem = (currentIndex) => {
+		setListTarea(listTarea.filter((item, index ) => index !== currentIndex))
+	}
 	return (
-		<div className="text-center">
-            
-
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="mx-auto" style={{width: "25%"}}>
+			<h1 className="text-center fs-1">TODO</h1>
+			<form onSubmit={(event) => {
+				event.preventDefault()
+				setListTarea([item, ...listTarea])
+				setItem("")
+				add()
+				
+			}}>
+				<input type="text"
+					onChange={handleChange}
+					value={item} className="form-control"
+					placeholder="Insert Tarea"
+				/>
+			</form>
+			<div>
+				<ul className="list-group" onChange={setCounter}>
+					{listTarea.map((newItem, index) => { 
+						return(
+						<li key={crypto.randomUUID()} style={estilo} onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}  className="list-group-item d-flex justify-content-between">
+							{newItem}
+							<button className="btn  " onClick={() =>{
+							deleteItem(index);
+							sub()
+							}}>
+								<i className=" text-light fa-solid fa-x" ></i>
+							</button>
+						</li>
+						)
+					})}
+				</ul>
+			</div>
+			<div className="border rounded">{counter} Pending Tasks</div>
 		</div>
-	);
-};
-
-export default Home;
+	)
+}
