e35940cdd6f85de8aa18240466b72c6a84958c06
BasCart <b.cartagena.paredes@gmail.com>
2025-04-10 20:19:10 +0000

    anchor to api

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,31 +1,81 @@
-import React, { useState } from "react";
-
+import React, { useEffect, useState } from "react";
+import { getApi, upTask, deleteItemOnApi, newUser, deletUser } from "./workinAPI";
 //create your first component
 export const Home = () => {
 	let [item, setItem] = useState("")
 	let [listTarea, setListTarea] = useState([])
-	const[hover, setHover]= useState(false);
-	const estilo = { backgroundColor: hover ? "lightgray" : "rgb(248, 249, 250)" }
 	const[counter, setCounter] = useState(0)
-	const add=()=> {setCounter ((prevCounter) => prevCounter + 1)}
-	const sub=()=> {setCounter ((prevCounter) => prevCounter - 1)}
 	
 	const handleChange = (event) => {
 		setItem(event.target.value)
 	}
-	const deleteItem = (currentIndex) => {
-		setListTarea(listTarea.filter((item, index ) => index !== currentIndex))
+	const deleteTheUser = async()=>{
+		try{
+			await deletUser();
+			await newUser();
+			await loadTask();
+		} catch (error){
+			console.log("error")
+		}
 	}
+	
+	const deleteItem = async(idToDelete)=>{
+		await deleteItemOnApi(idToDelete);
+		const updateTask = await getApi();
+		if (updateTask && updateTask.todos && Array.isArray(updateTask.todos)){
+			setListTarea(updateTask.todos);
+			setCounter(updateTask.todos.length);
+		} else {
+			console.error("Error: getApi en deleteItem no devolvió un array:", updateTask);
+		}
+	}
+
+	const aNewTask = async(theTask)=>{
+		theTask.preventDefault()
+		const trimmedItem = item.trim()
+		if(trimmedItem !== ""){
+			await upTask(trimmedItem);
+			const updateTask = await getApi();
+			if (updateTask && updateTask.todos && Array.isArray(updateTask.todos)){
+				setListTarea(updateTask.todos)
+				setCounter(updateTask.todos.length)
+				setItem("")
+			} else {
+				console.error("Error: getApi en aNewTask no devolvió un array:", updateTask);
+			}
+		}else{
+			alert("Please, insert a new Tarea")
+		}
+	}
+	const loadTask = async ()=>{
+		const theTasks = await getApi();
+		if (theTasks && theTasks.todos && Array.isArray(theTasks.todos)){
+			setListTarea(theTasks.todos)
+			setCounter(theTasks.todos.length)
+		} else {
+			await newUser()
+			console.error("Error, getApi no devolvio un array.", theTasks);
+			setListTarea([])
+			setCounter(0)
+			await loadTask()
+		}
+	}
+	useEffect(()=>{
+		loadTask();
+	}, [])
+	const activateHover = (evento) => {
+		evento.currentTarget.style.backgroundColor = "grey";
+		
+	};
+	
+	const nonHover = (evento) => {
+		evento.currentTarget.style.backgroundColor = "white";
+		
+	};
 	return (
 		<div className="mx-auto" style={{width: "25%"}}>
 			<h1 className="text-center fs-1">TODO</h1>
-			<form onSubmit={(event) => {
-				event.preventDefault()
-				setListTarea([item, ...listTarea])
-				setItem("")
-				add()
-				
-			}}>
+			<form onSubmit={aNewTask}>
 				<input type="text"
 					onChange={handleChange}
 					value={item} className="form-control"
@@ -35,13 +85,11 @@ export const Home = () => {
 			<div>
 				<ul className="list-group" onChange={setCounter}>
 					{listTarea.map((newItem, index) => { 
+						
 						return(
-						<li key={crypto.randomUUID()} style={estilo} onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}  className="list-group-item d-flex justify-content-between">
-							{newItem}
-							<button className="btn  " onClick={() =>{
-							deleteItem(index);
-							sub()
-							}}>
+						<li key={newItem.id} onMouseEnter={activateHover} onMouseLeave={nonHover}  className="list-group-item d-flex justify-content-between">
+							{newItem.label}
+							<button className="btn" onClick={() =>{deleteItem(newItem.id)}}>
 								<i className=" text-light fa-solid fa-x" ></i>
 							</button>
 						</li>
@@ -49,7 +97,13 @@ export const Home = () => {
 					})}
 				</ul>
 			</div>
-			<div className="border rounded">{counter} Pending Tasks</div>
+			{counter === 0 ? <div className="border rounded" style={{width: "100%"}}> &nbsp;You don't have Tareas</div>: null}
+			<div className="border rounded d-flex justify-content-between align-items-center" onMouseEnter={activateHover} onMouseLeave={nonHover} >
+				&nbsp;{counter} Pending Tasks
+				{counter !== 0 ? <button type="button" className="btn" onClick={deleteTheUser}>
+					<i className=" text-light fa-solid fa-x"></i>&nbsp;&nbsp;&nbsp;&nbsp;
+				</button>:null}
+			</div>
 		</div>
 	)
 }
